d99ec293d923e773c9d66715ac5b8a64aefa4960
Tazrian <tazrianislam06@gmail.com>
2026-03-01 10:09:15 +0600

    added responsive design for placeorder page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,28 +1,31 @@
-import React, { useState } from 'react'
-import { Navbar } from './components/navbar/Navbar'
-import { Routes, Route } from 'react-router-dom'
-import Home from './pages/Home/Home'
-import Cart from './pages/Cart/Cart'
-import PlaceOrder from './pages/PlaceOrder/PlaceOrder'
-import Footer from './components/Footer/Footer'
-import LogInPopUp from './components/LogInPopUp/LogInPopUp'
+import React, { useState } from "react";
+import { Navbar } from "./components/navbar/Navbar";
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home/Home";
+import Cart from "./pages/Cart/Cart";
+import PlaceOrder from "./pages/PlaceOrder/PlaceOrder";
+import Footer from "./components/Footer/Footer";
+import LogInPopUp from "./components/LogInPopUp/LogInPopUp";
+import './App.css'
 
 const App = () => {
-  const[showLogin, setShowLogin] = useState(false)
+  const [showLogin, setShowLogin] = useState(false);
   return (
     <>
-      {showLogin ? <LogInPopUp setShowLogin={setShowLogin} />:<></>}
-      <div className='app'>
-        <Navbar setShowLogin = {setShowLogin} />
-        <Routes>
-          <Route path='/' element={<Home />} />
-          <Route path='/cart' element={<Cart />} />
-          <Route path='/order' element={<PlaceOrder />} />
-        </Routes>
+      {showLogin ? <LogInPopUp setShowLogin={setShowLogin} /> : <></>}
+      <div className="app">
+        <Navbar setShowLogin={setShowLogin} />
+        <div className="main">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/cart" element={<Cart />} />
+            <Route path="/order" element={<PlaceOrder />} />
+          </Routes>
+        </div>
+        <Footer />
       </div>
-      <Footer />
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
